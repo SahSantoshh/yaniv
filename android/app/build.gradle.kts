53880ffc2f11e9c0393753458f3244d4f66926e9
Santosh Prasad Sah <sahsantoshh@gmail.com>
2025-08-22 21:23:36 +0545

    khai

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -65,12 +65,12 @@ android {
     }
 }
 
-flutter {
-    source = "../.."
-}
-
 dependencies {
     implementation("com.google.android.play:core:1.10.3")
     implementation("com.google.android.play:core-ktx:1.8.1")
+}
+
+flutter {
+    source = "../.."
 }
 
