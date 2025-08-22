dea28b7fe99e15f4818b6dda623753522ac964a0
Santosh Prasad Sah <sahsantoshh@gmail.com>
2025-08-22 22:27:39 +0545

    feat: Integrate in-app updates and feature delivery
    This commit integrates the Google Play Core Library to enable in-app updates and on-demand feature delivery.
    
    Key changes:
    - Updated `pubspec.yaml` to version `1.0.0+2`.
    - Updated Android build configuration in `android/app/build.gradle.kts`.
    - Replaced Play Core libraries `com.google.android.play:core:1.10.3` and `com.google.android.play:core-ktx:1.8.1` with the following:
        - `com.google.android.play:app-update:2.1.0`
        - `com.google.android.play:app-update-ktx:2.1.0`
        - `com.google.android.play:feature-delivery:2.1.0`
        - `com.google.android.play:feature-delivery-ktx:2.1.0`

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,8 +66,10 @@ android {
 }
 
 dependencies {
-    implementation("com.google.android.play:core:1.10.3")
-    implementation("com.google.android.play:core-ktx:1.8.1")
+    implementation("com.google.android.play:app-update:2.1.0")
+    implementation("com.google.android.play:app-update-ktx:2.1.0")
+    implementation("com.google.android.play:feature-delivery:2.1.0")
+    implementation("com.google.android.play:feature-delivery-ktx:2.1.0")
 }
 
 flutter {
